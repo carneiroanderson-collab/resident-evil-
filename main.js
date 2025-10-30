f3a2913f3f4fb96e5c3d0c6e23c3ec555557eabe
carneiroanderson-collab <carneiro.anderson@escola.pr.gov.br>
2025-10-30 09:41:11 -0300

    h

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(avanca)
 
 avanca.forEach(button =>{
     button.addEventListener('click' , function(){
-       const atual = document.querySelector('ativo')
+       const atual = document.querySelector('.ativo')
        const proximosso="passo-"+ this.getAttribute('data-proximo')
        console.log(proximosso, atual)
        atual.classList.remove('ativo')
